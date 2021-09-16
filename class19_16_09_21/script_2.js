ab3b5685d3c85ad66900de5c484c60dcc7baa3f3
KARTHIKEYAN GANESH <37135347+kn123g@users.noreply.github.com>
2021-09-16 11:59:09 +0530

    added bootsrap

--- class19_16_09_21/script_2.js
+++ class19_16_09_21/script_2.js
@@ -1,8 +1,17 @@
+document.getElementById("submit").addEventListener("click", function(event) {
+    addCountry();
+    event.preventDefault();
+});
+
 function addCountry() {
     let option = document.createElement('option');
     let newCountry = document.getElementById('countryName').value;
     option.value = newCountry;
     option.innerHTML = newCountry;
-    document.getElementById('country').appendChild(option);
-    document.getElementById('countryName').value = '';
+    if (newCountry != '') {
+
+        document.getElementById('country').appendChild(option);
+        document.getElementById('countryName').value = '';
+
+    }
 }
