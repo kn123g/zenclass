e13ebaf33502792c9087d1575efc76c0a1e858ee
KARTHIKEYAN GANESH <37135347+kn123g@users.noreply.github.com>
2021-09-17 12:10:33 +0530

    added table insert

--- class20_17_09_21/demo1/script.js
+++ class20_17_09_21/demo1/script.js
@@ -1,14 +1,29 @@
-let div1 = document.getElementById('div1');
-let div2 = document.getElementById('div2');
+// let div1 = document.getElementById('div1');
+// let div2 = document.getElementById('div2');
 
-div1.addEventListener('click', (e) => {
-    alert('div1 clicked')
-    console.log(e)
-})
+// div1.addEventListener('click', (e) => {
+//     alert('div1 clicked')
+//     console.log(e)
+// }, true)
 
 
-div2.addEventListener('click', (e) => {
+// div2.addEventListener('click', (e) => {
 
-    alert('div2 clicked')
-    console.log(e)
-})
+//     alert('div2 clicked')
+//     console.log(e)
+// }, true)
+
+function AddRow() {
+    let firstName = document.getElementById('fname').value;
+    let lastName = document.getElementById('lname').value;
+    let email = document.getElementById('email').value;
+    let age = document.getElementById('age').value;
+
+    let table = document.getElementById('show');
+    let row = table.insertRow(-1);
+    row.insertCell(-1).append(firstName);
+    row.insertCell(-1).append(lastName);
+    row.insertCell(-1).append(email);
+    row.insertCell(-1).append(age);
+
+}
