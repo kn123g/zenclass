bf0acea6009a422381235a3bcc6f7c0a598326db
KARTHIKEYAN GANESH <37135347+kn123g@users.noreply.github.com>
2021-09-23 12:54:57 +0530

    random img using fetch API

--- class24_23_09_21/exercise/fetch_API_.js
+++ class24_23_09_21/exercise/fetch_API_.js
@@ -1,6 +1,8 @@
-fetch('https://picsum.photos/200')
-    .then(res => {
-        console.log(res)
-        document.getElementById('img').src = res.url
-    })
-    .catch(error => console.log(error))
+function randomImg() {
+    fetch('https://picsum.photos/200')
+        .then(res => {
+            console.log(res)
+            document.getElementById('img').src = res.url
+        })
+        .catch(error => console.log(error))
+}
