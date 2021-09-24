cff77232a9330f964edaaf1336faaab86658f396
KARTHIKEYAN GANESH <37135347+kn123g@users.noreply.github.com>
2021-09-24 11:13:16 +0530

    addded header

--- class25_24_09_21/asyn_await_fetch/script.js
+++ class25_24_09_21/asyn_await_fetch/script.js
@@ -1,13 +1,23 @@
 async function fetchContent() {
-    let data = await fetch('https://jsonplaceholder.typicode.com/users');
-    let jsonData = await data.json();
-    console.log(jsonData)
-    let table = document.getElementById('table');
-    for (let content in jsonData) {
-        console.log(table)
-        let row = table.insertRow(-1);
-        row.insertCell(-1).append(jsonData[content].name);
-        row.insertCell(-1).append(jsonData[content].address.city);
+    try {
+        let data = await fetch('https://jsonplaceholder.typicode.com/users');
+        let jsonData = await data.json();
+        console.log(jsonData)
+        let table = document.getElementById('table');
+        let header = document.createElement('th');
+        header.innerHTML = 'Name'
+        table.querySelector('thead').appendChild(header)
+        header = document.createElement('th');
+        header.innerHTML = 'City'
+        table.querySelector('thead').appendChild(header)
+        for (let content in jsonData) {
+            console.log(table)
+            let row = table.insertRow(-1);
+            row.insertCell(-1).append(jsonData[content].name);
+            row.insertCell(-1).append(jsonData[content].address.city);
+        }
+    } catch (e) {
+
     }
 
 }
