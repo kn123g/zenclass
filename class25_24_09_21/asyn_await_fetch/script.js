723f02720c131e8ad3ba2d0bb19c5eb89bbb5ff8
KARTHIKEYAN GANESH <37135347+kn123g@users.noreply.github.com>
2021-09-24 11:27:58 +0530

    splited update dom

--- class25_24_09_21/asyn_await_fetch/script.js
+++ class25_24_09_21/asyn_await_fetch/script.js
@@ -1,25 +1,30 @@
+let url = `https://jsonplaceholder.typicode.com/users`
+
 async function fetchContent() {
     try {
-        let data = await fetch('https://jsonplaceholder.typicode.com/users');
+        let data = await fetch(url);
         let jsonData = await data.json();
         console.log(jsonData)
-        let table = document.getElementById('table');
-        let header = document.createElement('th');
-        header.innerHTML = 'Name'
-        table.querySelector('thead').appendChild(header)
-        header = document.createElement('th');
-        header.innerHTML = 'City'
-        table.querySelector('thead').appendChild(header)
-        for (let content in jsonData) {
-            console.log(table)
-            let row = table.insertRow(-1);
-            row.insertCell(-1).append(jsonData[content].name);
-            row.insertCell(-1).append(jsonData[content].address.city);
-        }
+        updateContentToDOM(jsonData)
     } catch (e) {
         let errorElement = document.createElement('p')
         errorElement.innerHTML = e;
         document.getElementsByClassName('container')[0].appendChild(errorElement);
     }
+}
 
+function updateContentToDOM(jsonData) {
+    let table = document.getElementById('table');
+    let header = document.createElement('th');
+    header.innerHTML = 'Name'
+    table.querySelector('thead').appendChild(header)
+    header = document.createElement('th');
+    header.innerHTML = 'City'
+    table.querySelector('thead').appendChild(header)
+    for (let content in jsonData) {
+        console.log(table)
+        let row = table.insertRow(-1);
+        row.insertCell(-1).append(jsonData[content].name);
+        row.insertCell(-1).append(jsonData[content].address.city);
+    }
 }
