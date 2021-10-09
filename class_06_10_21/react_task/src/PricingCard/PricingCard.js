0ed6c8ba44a0bc25c6bc8a6770e8ce18db19967c
KARTHIKEYAN GANESH <37135347+kn123g@users.noreply.github.com>
2021-10-09 12:14:10 +0530

    replaced a href with button to avoid deployment error and replaced all class to className

--- class_06_10_21/react_task/src/PricingCard/PricingCard.js
+++ class_06_10_21/react_task/src/PricingCard/PricingCard.js
@@ -4,22 +4,22 @@ import "@fortawesome/fontawesome-free/css/all.css";
 class PricingCard extends Component {
   render() {
     return (
-      <div class="col-lg-4">
-        <div class="card mb-5 mb-lg-0">
-          <div class="card-body">
-            <h5 class="card-title text-muted text-uppercase text-center">
+      <div className="col-lg-4">
+        <div className="card mb-5 mb-lg-0">
+          <div className="card-body">
+            <h5 className="card-title text-muted text-uppercase text-center">
               {this.props.content.title}
             </h5>
-            <h6 class="card-price text-center">
+            <h6 className="card-price text-center">
               <span>{this.props.content.pricing.currency}</span>
               {this.props.content.pricing.price}
-              <span class="period">/{this.props.content.pricing.duration}</span>
+              <span className="period">/{this.props.content.pricing.duration}</span>
             </h6>
             <hr />
-            <ul class="fa-ul">
-              {this.props.content.details.map((li) => {
+            <ul className="fa-ul">
+              {this.props.content.details.map((li,index) => {
                 return (
-                  <li className={li.apply ? null : "text-muted"}>
+                  <li className={li.apply ? null : "text-muted"} key={index}>
                     <span className="fa-li">
                       <i
                         className={li.apply ? "fas fa-check" : "fas fa-times"}
@@ -30,10 +30,11 @@ class PricingCard extends Component {
                 );
               })}
             </ul>
-            <div class="d-grid">
-              <a href="#" className="btn btn-primary text-uppercase">
+            <div className="d-grid">
+              {/* replaced a href with button because while deployment getting no valid href url error */}
+              <button className="btn btn-primary text-uppercase">
                 Button
-              </a>
+              </button>
             </div>
           </div>
         </div>
