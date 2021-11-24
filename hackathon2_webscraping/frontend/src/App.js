2d30ec65ab1a226059f00631ed92819a468f57e4
KARTHIKEYAN GANESH <karthiknganesh96@gmail.com>
2021-11-24 19:26:57 +0530

    added pagination

--- hackathon2_webscraping/frontend/src/App.js
+++ hackathon2_webscraping/frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/Header'
 import ProductCard from './components/ProductCard/ProductCard'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import axios from 'axios';
 import Grid from '@mui/material/Grid';
 import {backend_base_url} from './constants/external_api';
@@ -11,26 +11,39 @@ import Box from '@mui/material/Box';
 function App() {
   const [products,setProducts] = useState([]);
   const [page, setPage] = useState(1);
+  const [products_count,setPrductsCount] = useState(0);
+  const [search,setSearch] = useState();
   const handleChange = (event, value) => {
     setPage(value);
   };
 
-  const searchHandle =(search)=>{
-    axios.get(`${backend_base_url}products/${search}`)
+  const searchHandle =(search_text)=>{
+    setSearch(search_text);
+    axios.get(`${backend_base_url}products/${search_text}/${page}`)
     .then((response)=>{
       setProducts(response.data.products)
     })
     .catch(()=>{
 
     });
-    axios.get(`${backend_base_url}countproducts/${search}`)
+    axios.get(`${backend_base_url}countproducts/${search_text}`)
     .then((response)=>{
-      // setProducts(response.data.products)
+      setPrductsCount(response.data.count)
     })
     .catch(()=>{
 
     });
   }
+
+  useEffect(()=>{
+    axios.get(`${backend_base_url}products/${search}/${page}`)
+    .then((response)=>{
+      setProducts(response.data.products)
+    })
+    .catch(()=>{
+
+    });
+  },[page,search])
   return (
         <>
           <Header searchHandle={searchHandle}/>
@@ -48,7 +61,7 @@ function App() {
           bgcolor: 'background.paper',
         }}
       >
-         {products.length > 0 ? <Pagination count={10} page={page} onChange={handleChange} color="primary" /> : null }
+         {products.length > 0 ? <Pagination count={Math.floor(products_count/12)} page={page} onChange={handleChange} color="primary" /> : null }
       </Box>
         </>
   )}
