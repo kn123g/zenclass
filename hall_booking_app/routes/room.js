29fef6615f7be410fc522ea9ed19072dc09c751d
karthi-12690 <karthi-12690@csez.zohocorpin.com>
2022-01-21 15:36:25 +0530

    addded postman api doc to home page

--- hall_booking_app/routes/room.js
+++ hall_booking_app/routes/room.js
@@ -6,6 +6,10 @@ router.post("/createroom",roomController.createRoom);
 router.post("/bookroom",roomController.bookRoom);
 router.get("/rooms",roomController.listRooms);
 router.get("/customers",roomController.listCustomers);
+
+router.get("/",(req,res)=>{
+    res.status(200).json({"api_docs":process.env.postman_doc})
+});
 // router.get("/",roomController.createRoom);
 
 module.exports = router;
