4a5531a80d664c7f89905622a8aacbe4cbc4db21
karthi-12690 <karthi-12690@csez.zohocorpin.com>
2022-01-21 18:03:33 +0530

    json parse added

--- nodejs-filesystem/app.js
+++ nodejs-filesystem/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser  = require("body-parser");
 const filesystemrouter = require('./routes/filesystem');
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log('server running on port',process.env.PORT || 3000)
 })
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/',filesystemrouter);
